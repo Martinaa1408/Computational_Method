3e661bc6e3238840f3e2301b3c26a75f45d3fd3f
Martina Castellucci <martina.castellucci@studio.unibo.it>
2025-06-01 09:42:20 +0200

    Create exam10.02A.py

---
+++ Lanese/script/exam10.02A.py
@@ -0,0 +1,9 @@
+#EXERCISE A
+#write a python program that takes from the user strings containing comma-separated integers. 
+#The program takes a command line parameters an integer threshold and the name of a destination file to write teh results. For each line, the program prints on the destination file the number of pairs of integers in the line whose product is strictly greater than the threshold.
+#sample execution:
+#threshold: 18
+#input:     #output file
+#3,4,5      1
+#4,4,23,1   3
+#-3,8,1     0
