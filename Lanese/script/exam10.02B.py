fa5d4f4802a425d9621b7b1a8f672f381264521d
Martina Castellucci <martina.castellucci@studio.unibo.it>
2025-06-01 09:46:26 +0200

    Create exam10.02B.py

---
+++ Lanese/script/exam10.02B.py
@@ -0,0 +1,14 @@
+#EXERCISE B
+#Write a python program that takes from file 'guess.txt' a single line containing space-separated integers. 
+#The user plays the following  game: he insert a number, and needs to guess the sum of two numbers in the line. 
+#If the guess is correct, he is congratulated with 'You win' and the game ends. If the guess is wrong, he has to retry but gets: 'upper' if all possible sums are higher than the guessed number, 'lower' if tehy are all lower and 'try again' otherwise
+#sample execution
+#input file:
+#4 12 13 5
+#run
+#8
+#upper
+#22
+#try again
+#25
+#you win
