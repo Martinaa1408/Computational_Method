93a6a36e0e2aa99ddd70abd79eb0f56dd78cba94
Martina Castellucci <martina.castellucci@studio.unibo.it>
2025-12-06 11:19:33 +0100

    Refactor Seq class and clean up comments

--- Manfredi/scripts/10.02B.py
+++ Manfredi/scripts/10.02B.py
@@ -1,13 +1,12 @@
 from exammm import SubstitutionMatrix
 from exammm import SequencePair
-from exammm import AlignedSequences  # Importiamo AlignedSequences
-#oppure: from exammm import SubstitutionMatrix, SequencePair, AlignedSequences
+from exammm import AlignedSequences  
 
-class Seq(SequencePair): #ok
-    def __init__(self, seq1, seq2): #ok
-        super().__init__(seq1, seq2) #ok
+class Seq(SequencePair):
+    def __init__(self, seq1, seq2):
+        super().__init__(seq1, seq2)
 
-    def alignment(self, matrix, gap, algorithm): #trattare in maniera più intelligente algorithm
+    def alignment(self, matrix, gap, algorithm): 
         F, T = self._create_matrices(matrix, gap, algorithm)
 
         if algorithm == "nw":
@@ -35,18 +34,18 @@ class Seq(SequencePair): #ok
 
         elif algorithm == "sw":
             max_score = -1
-            start_row, start_col = 0, 0 #difatti non c'è il bisogno di richiamare le lunghezze delle sequenze
+            start_row, start_col = 0, 0 
             for i in range(len(self.seq1) + 1):
                 for j in range(len(self.seq2) + 1):
                     if F[i][j] > max_score:
                         max_score = F[i][j]
                         start_row, start_col = i, j
 
-            i, j = start_row, start_col #indentazione corretta
+            i, j = start_row, start_col 
             aln1 = ""
             aln2 = ""
 
-            while T[i][j] != '': #ricordati il diverso
+            while T[i][j] != '':
                 if T[i][j] == 'd':
                     aln1 = self.seq1[i - 1] + aln1
                     aln2 = self.seq2[j - 1] + aln2
@@ -75,45 +74,3 @@ if __name__ == '__main__':
     print("Local Alignment (Smith-Waterman):")
     print(result_sw)
 
-#Verifica dei requisiti della traccia
-#Importazione del modulo
-#✅ Il codice importa correttamente le classi dal modulo exammm (che sembra corrispondere al file B_exam_text.py).
-
-#Estensione della classe SequencePair
-#✅ La classe Seq estende SequencePair e aggiunge il metodo alignment() per calcolare l’allineamento.
-
-#Il metodo alignment() accetta i tre parametri richiesti
-#✅ Il metodo alignment() prende in input:
-
-#Un oggetto SubstitutionMatrix per la matrice di sostituzione.
-#Un intero negativo come gap penalty.
-#Una stringa ("nw" o "sw") per selezionare l’algoritmo.
-#Il metodo restituisce un oggetto AlignedSequences
-#✅ Il metodo alignment() restituisce correttamente un'istanza di AlignedSequences.
-
-#Main che testa il codice
-#✅ Il codice nel blocco if __name__ == '__main__': esegue i seguenti passi:
-
-#Crea un oggetto SubstitutionMatrix caricando PAM250.txt.
-#Crea un oggetto Seq con le sequenze AAAAACCCC e RRRRCCCC.
-#Calcola sia un allineamento globale (nw) che un allineamento locale (sw).
-#Stampa i risultati.
-
-#Objects are an abstraction including both some data and the functions to work on them (called methods)
-#A class describes a type of object
-#One can define a class and then instantiate objects from it
-#We have to define:
-# How to create objects (constructor)
-# How to work on them (methods)
-#Classes are blocks which start with the class keyword, followed by the class name
-#They define a number of functions, called methods
-#The first parameter of each method is self, which refers to the current object of the class
-#Variables prefixed by self are instance variables, there is one copy per object of the class
-# The constructor initializes the instance variables of the class
-#Instance variables should not be used directly from outside the class, only via methods
-#This allows one to change the internal representation of the class without the need to change programs using it
-#This is called encapsulation
-#You may define classes starting from other classes
-#Subclasses have their own constructor
-#Subclasses inherit instance variables and methods from the superclass
-#Subclasses may have additional instance variables
