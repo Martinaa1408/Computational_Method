dc504f4430733ddf59200e47eefc11e7993ca940
Martina Castellucci <martina.castellucci@studio.unibo.it>
2025-12-06 11:20:12 +0100

    Clean up comments and output in 25.02.py
    Removed comments and a print statement for clarity.

--- Manfredi/scripts/25.02.py
+++ Manfredi/scripts/25.02.py
@@ -20,14 +20,11 @@ class SequencePair(input.SequencePair):
             self.seq2 = sequences.readline().strip()
 
     def _create_matrices(self, matrix):
-        # crea la matrice
         F = []
         T = []
-        # prima riga e prima colonna
         for i in range(len(self.seq2) + 1):
             F.append([0] * (len(self.seq1) + 1))
             T.append((['']) * (len(self.seq1) + 1))
-        # restante parte della matrice
         for i in range(1, len(self.seq2) + 1):
             for j in range(1, len(self.seq1) + 1):
                 top = F[i - 1][j] + matrix[self.seq1[j - 1], '-'], 'u'
@@ -40,6 +37,5 @@ class SequencePair(input.SequencePair):
 
 if __name__ == "__main__":
     myobj = SequencePair("input.fasta")
-    # print("la sequenza 1 da allineare è:", myobj.seq1, "||| con la sequenza 2 che è:", myobj.seq2)
     matrix = SubstitutionMatrix("PAM250.txt")
     print(myobj.smith_waterman(matrix))
